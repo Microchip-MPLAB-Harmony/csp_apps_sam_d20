c66ad8bce2b53a4b1d9690608361458f397d2d25
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 16:40:01 +0530

    [TC] Regenerated demo for tc_tc_compare_mode_sam_d20_xpro
    MH3-78957

--- apps/tc/tc_compare_mode/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_d20_xpro/peripheral/clock/plib_clock.c
@@ -67,7 +67,7 @@ static void DFLL_Initialize(void)
     }
 
     /*Load Calibration Value*/
-    uint8_t calibCoarse = (uint8_t)(((*(uint32_t*)0x806024U) >> 26U ) & 0x3fU);
+    uint8_t calibCoarse = (uint8_t)(((*((uint32_t*)0x00806020U + 1U)) >> 26U ) & 0x3fU);
     calibCoarse = (((calibCoarse) == 0x3FU) ? 0x1FU : (calibCoarse));
 
     SYSCTRL_REGS->SYSCTRL_DFLLVAL = SYSCTRL_DFLLVAL_COARSE((uint32_t)calibCoarse) | SYSCTRL_DFLLVAL_FINE((uint32_t)512U);
@@ -90,7 +90,7 @@ static void DFLL_Initialize(void)
     {
         /* Waiting for DFLL to fully lock to meet clock accuracy */
     }
-
+    
 }
 
 
@@ -116,6 +116,9 @@ static void GCLK1_Initialize(void)
         /* wait for the Generator 1 synchronization */
     }
 }
+
+
+
 
 void CLOCK_Initialize (void)
 {
@@ -144,5 +147,7 @@ void CLOCK_Initialize (void)
 
     /* Configure the APBC Bridge Clocks */
     PM_REGS->PM_APBCMASK = 0x15800U;
+
+
 
 }
