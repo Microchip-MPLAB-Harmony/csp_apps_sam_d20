a2748378d146634e5125cf05532974ab69b14c5e
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 16:39:58 +0530

    [SYSTICK] Regenerated demo for systick_systick_periodic_timeout_sam_d20_xpro
    MH3-78955

--- apps/systick/systick_periodic_timeout/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
+++ apps/systick/systick_periodic_timeout/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
@@ -71,10 +71,10 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_DIR = 0x4000;
-   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x0;
+   PORT_REGS->GROUP[0].PORT_DIR = 0x4000U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x0U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
 
