dfbce5fc893cf6f9d44099b926eaf482c81d4f8d
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-17 16:39:44 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_d20_xpro
    MH3-78945

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
@@ -71,14 +71,14 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_DIR = 0x4000;
-   PORT_REGS->GROUP[0].PORT_OUT = 0x4000;
-   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x0;
+   PORT_REGS->GROUP[0].PORT_DIR = 0x4000U;
+   PORT_REGS->GROUP[0].PORT_OUT = 0x4000U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x0U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x33;
-   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x33U;
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
 
