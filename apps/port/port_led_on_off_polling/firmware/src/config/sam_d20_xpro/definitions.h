67be8f0df3c071bdad353b5911d16f448f3109ed
Pranjal Das <pranjal.das@microchip.com>
2024-10-18 15:45:20 +0530

    [PORT] Regenerated demo for port__port_led_on_off_polling__firmware__sam_d20_xpro.X
    MH3-101580

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_d20_xpro/definitions.h
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_d20_xpro/definitions.h
@@ -63,13 +63,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMD20J18"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMD"
-#define DEVICE_SERIES		 "SAMD20"
+#define DEVICE_NAME          "ATSAMD20J18"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMD"
+#define DEVICE_SERIES        "SAMD20"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
